32076a38d2c31f7892ab0300fcba2feaf267d77d
Your Name <you@example.com>
2022-06-21 14:12:51 +0800

    updaet

--- array.go
+++ array.go
@@ -1,7 +1,7 @@
 package goutil
 
-func ArrayIn(target interface{}, array []interface{}) bool {
-	for _, v := range array {
+func ArrayIn(target interface{}, array interface{}) bool {
+	for _, v := range array.([]interface{}) {
 		if v == target {
 			return true
 		}
