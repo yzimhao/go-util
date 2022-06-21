43b338fc191d5a118df7f5220ba7b2cbb3550169
Your Name <you@example.com>
2022-06-21 11:47:29 +0800

    init

---
+++ config.go
@@ -0,0 +1,12 @@
+package goutil
+
+import "github.com/spf13/viper"
+
+func ViperInit(configFile string) *viper.Viper {
+	viper.SetConfigFile(configFile)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	return viper.GetViper()
+}
---
+++ version.go
@@ -0,0 +1,18 @@
+package goutil
+
+import (
+	"fmt"
+	"os"
+)
+
+var (
+	VERSION     = "v0.0.0"
+	BUILD       = "2016-11-18 16:40:00"
+	COMMIT_SHA1 = ""
+	GO_VERSION  = ""
+)
+
+func ShowVersion() {
+	fmt.Printf("version: %s\nbuild: %s\ncommit: %s\n%s\n", VERSION, BUILD, COMMIT_SHA1, GO_VERSION)
+	os.Exit(0)
+}
