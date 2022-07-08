2351ebf1134c0bebca671162db5e2758cdf10bf5
Your Name <you@example.com>
2022-07-08 10:24:36 +0800

    add path.go

---
+++ path.go
@@ -0,0 +1,15 @@
+package utilgo
+
+import "os"
+
+func PathExist(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+
+	if os.IsNotExist(err) {
+		return false
+	}
+	return false
+}
